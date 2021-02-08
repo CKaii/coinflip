0b06199357b45b4e3a89237b71fc6a6ac6ed50dd
unknown <kevin.cheung.kai@gmail.com>
2021-02-08 00:20:41 -0500

    Added a counter for number of heads and tails below the coin

--- src/components/Coinflip.jsx
+++ src/components/Coinflip.jsx
@@ -1,27 +1,33 @@
 import React, { useState } from 'react';
 
 function Coinflip() {
-  const [flip, setFlip] = useState(0);
-  let heads = 0;
-  let tails = 0;
-  let total = heads + tails;
-
-  if (flip === 0) {
-    heads += 1;
-  } else if (flip === 1) {
-    tails += 1;
-  }
+  const [flip, setFlip] = useState({ heads: 0, tails: 0 });
 
   function handleClick() {
     let randomNumber = Math.floor(Math.random() * 2);
-    setFlip(randomNumber);
+
+    setFlip((prevFlip) => {
+      if (randomNumber === 0) {
+        return {
+          heads: flip.heads + 1,
+          tails: prevFlip.tails,
+        };
+      } else if (randomNumber === 1) {
+        return {
+          heads: prevFlip.heads,
+          tails: flip.tails + 1,
+        };
+      }
+    });
+
+    // setFlip(randomNumber);
     console.log(randomNumber);
   }
 
   return (
     <div>
       <h1>Coinflip Simulator</h1>
-      {flip === 0 ? (
+      {flip.heads === 0 ? (
         <img
           className="coinImage"
           src={'https://tinyurl.com/react-coin-heads-jpg'}
@@ -32,9 +38,11 @@ function Coinflip() {
           src={'https://i.ebayimg.com/images/g/wGEAAOSwYu1crzzn/s-l400.jpg'}
         />
       )}
+      <br />
       <button onClick={handleClick}>Flip Coin</button>
       <p>
-        Out of {total} flips, there have been {heads} heads and {tails} tails
+        Out of 0 flips, there have been {flip.heads} heads and {flip.tails}{' '}
+        tails
       </p>
     </div>
   );
